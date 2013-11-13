f7fbabfe65541368157645b79d7d9e94298e4f6f
Dzakus <dzakus13@gmail.com>
2013-11-13 22:23:20 +0100

    Code cleanup

--- src/com/google/android/youtube/debug/Common.java
+++ src/com/google/android/youtube/debug/Common.java
@@ -1,20 +1,27 @@
 package com.google.android.youtube.debug;
 
 public class Common {
-	
+
 	public static final boolean DEBUG = BuildConfig.DEBUG;
 	public static final String PKG_NAME = Module.class.getPackage().getName();
 	public static final String PKG_NAME_GSF = "com.google.android.gsf";
 	public static final String PKG_NAME_YOUTUBE = "com.google.android.youtube";
-	
+
 	public static final String KEY_IS_BGOL_ENABLED = "is_bgol_enabled";
 	public static final String KEY_HIDE_WATERMARK = "hide_watermark";
 	public static final String KEY_SHOW_DOWNLOAD = "show_download";
 	public static final String KEY_MEDIA_PLAYER_DEBUG = "media_player_debug";
-		
+
 	public static final boolean DEF_IS_BGOL_ENABLED = false;
 	public static final boolean DEF_HIDE_WATERMARK = false;
 	public static final boolean DEF_SHOW_DOWNLOAD = false;
 	public static final boolean DEF_MEDIA_PLAYER_DEBUG = false;
-	
+
+	public static final boolean equalsWithNulls(Object a, Object b) {
+		if (a == b)
+			return true;
+		if ((a == null) || (b == null))
+			return false;
+		return a.equals(b);
+	}
 }
--- src/com/google/android/youtube/debug/Module.java
+++ src/com/google/android/youtube/debug/Module.java
@@ -22,20 +22,20 @@ public class Module implements IXposedHookLoadPackage {
 	public static final Uri URI_GSERIVCES = Uri.parse("content://com.google.android.gsf.gservices");
 	public static final Uri URI_GSERIVCES_PREFIX = Uri.parse("content://com.google.android.gsf.gservices/prefix");
 
-	private static final XSharedPreferences mPref = new XSharedPreferences(PKG_NAME);
+	private static final XSharedPreferences sPref = new XSharedPreferences(PKG_NAME);
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if(lpparam.packageName.equals(PKG_NAME_YOUTUBE)){
-			mPref.reload();
-			if(mPref.getBoolean(Common.KEY_HIDE_WATERMARK, Common.DEF_HIDE_WATERMARK)){
+			sPref.reload();
+			if(sPref.getBoolean(Common.KEY_HIDE_WATERMARK, Common.DEF_HIDE_WATERMARK)){
 				final Class<?> AnnotationOverlayClass = XposedHelpers.findClass(CLASS_ANNOTATION_OVERLAY, lpparam.classLoader);
 				XposedHelpers.findAndHookMethod(AnnotationOverlayClass, "d", XC_MethodReplacement.DO_NOTHING);
 			}
 
 		}else if (lpparam.packageName.equals(PKG_NAME_GSF)){
 			
-			mPref.reload();
+			sPref.reload();
 			final Class<?> ProviderClass = XposedHelpers.findClass(CLASS_PROVIDER, lpparam.classLoader);
 		
 				// query (Uri uri, String[] projection, String selection, String[]
@@ -43,21 +43,21 @@ public class Module implements IXposedHookLoadPackage {
 				XposedHelpers.findAndHookMethod(ProviderClass, "query", Uri.class, String[].class, String.class,
 					String[].class, String.class, new XC_MethodHook() {
 
-					/*@Override
-					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-						Object[] args = param.args;
-						XposedBridge.log(" query( "
-								+ "uri="+((Uri)args[0])
-								+ ", projection ="+Arrays.toString((String[])args[1])
-								+ ", selection ="+args[2]
-								+ ", selectionArgs = "+Arrays.toString((String[])args[3])
-								+ ", sortOrder="+args[4]
-								+ ");"
-						);
-					}*/
+						/*@Override
+						protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+							Object[] args = param.args;
+							XposedBridge.log(" query( "
+									+ "uri="+((Uri)args[0])
+									+ ", projection ="+Arrays.toString((String[])args[1])
+									+ ", selection ="+args[2]
+									+ ", selectionArgs = "+Arrays.toString((String[])args[3])
+									+ ", sortOrder="+args[4]
+									+ ");"
+							);
+						}*/
 						@Override
 						protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-							boolean isEnabledHook = mPref.getBoolean(KEY_IS_BGOL_ENABLED, true);
+							boolean isEnabledHook = sPref.getBoolean(KEY_IS_BGOL_ENABLED, true);
 							if (!isEnabledHook) {
 								return;
 							}
@@ -74,8 +74,8 @@ public class Module implements IXposedHookLoadPackage {
 							if (selectionArgs != null && selectionArgs.length > 0)
 								key = ((String[]) param.args[3])[0];
 
-							if (key != null & key.equals(KEY_IS_BGOL_ENABLED) & uri != null
-									& uri.compareTo(URI_GSERIVCES) == 0) {
+							if (Common.equalsWithNulls(key, KEY_IS_BGOL_ENABLED)
+									& Common.equalsWithNulls(uri, URI_GSERIVCES)) {
 								MatrixCursor fake_cursor = new MatrixCursor(new String[] { "key", "value" });
 								fake_cursor.addRow(new Object[] { KEY_IS_BGOL_ENABLED, "true" });
 								param.setResult(fake_cursor);
